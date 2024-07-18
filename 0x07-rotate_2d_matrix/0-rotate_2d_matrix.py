b26d14652abaa496e2fa7f92e729335fd040e61d
abdelrahman21-arch <boodyhussein12@gmail.com>
2024-07-18 20:53:37 +0300

    rotating by 2-D matrix by 90 degrees.

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+"""rotate 2d matrix by 90 degrees"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+
+    Args:
+        matrix:  the 2-D matrix to be rotated by 90 deg clockwise
+
+    Returns: Nothing
+
+    """
+
+    transpose = [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+
+    for row in transpose:
+        row.reverse()
+    matrix.clear()
+    matrix.extend(transpose)
